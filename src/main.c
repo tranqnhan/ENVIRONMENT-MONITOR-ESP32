24b797f5adcb7343e70728dcde121a64fb0e0f03
nhtran <nhtran@iastate.edu>
2026-09-13 13:33:58 -0500

    add task with freertos

--- src/main.c
+++ src/main.c
@@ -12,11 +12,14 @@
 #include "scd41.h"
 #include "display.h"
 
+#define DEBUG 1
 
 const char *SCD41_TAG = "SCD41";
 
 
 void handle_cmd(esp_err_t err, const char *cmd) {
+    if (!DEBUG) return;
+
     if (err != ESP_OK) {
         ESP_LOGE(SCD41_TAG, "%s command failed: %s.",
             cmd,
@@ -29,51 +32,7 @@ void handle_cmd(esp_err_t err, const char *cmd) {
 }
 
 
-void write_measurements(const measurement_t *m) {
-    ESP_LOGI(SCD41_TAG, "CO2: %d ppm | Temperature: %.2f C | Humidity: %.2f%%" ,
-        m->co2, m->temperature, m->humidity
-    );
-}
-
-
-// void scd41_task() {
-//     esp_err_t err;
-
-//     err = scd41_init();
-//     handle_cmd(err, "Initialize");
-
-//     err = scd41_start_periodic_measurement();
-//     handle_cmd(err, "Start periodic measurement");
-    
-//     uint32_t is_data_ready;
-//     measurement_t scd41_m;
-
-//     while (1) {
-//         is_data_ready = 0;
-//         err = scd41_data_ready(&is_data_ready);
-//         handle_cmd(err, "Data ready");
-
-//         if (is_data_ready) {
-//             err = scd41_read_measurement(&scd41_m);
-//             handle_cmd(err, "Read");
-//             write_measurements(&scd41_m);
-//         }
-
-//         vTaskDelay(pdMS_TO_TICKS(5000));
-//     }
-// }
-
-
-
-
-// ---------------------------------------------------------
-// Main
-// ---------------------------------------------------------
-void app_main(void)
-{
-    // Give USB Serial/JTAG time to connect
-    vTaskDelay(pdMS_TO_TICKS(1000));
-
+void setup() {
     esp_err_t err = i2c_init();
     handle_cmd(err, "i2c init");
 
@@ -85,9 +44,13 @@ void app_main(void)
 
     err = scd41_start_periodic_measurement();
     handle_cmd(err, "scd41 start periodic measurement");
-    
+}
+
+
+void physical_task(void *pvParameter) {
     uint32_t is_data_ready;
     measurement_t scd41_m;
+    esp_err_t err;
 
     while (1) {
         is_data_ready = 0;
@@ -97,10 +60,30 @@ void app_main(void)
         if (is_data_ready) {
             err = scd41_read_measurement(&scd41_m);
             handle_cmd(err, "scd41 read");
-            //write_measurements(&scd41_m);
             display_measurements_task(&scd41_m);
         }
 
         vTaskDelay(pdMS_TO_TICKS(5000));
     }
+
+}
+
+
+// ---------------------------------------------------------
+// Main
+// ---------------------------------------------------------
+void app_main(void)
+{
+    // Give USB Serial/JTAG time to connect
+    vTaskDelay(pdMS_TO_TICKS(1000));
+
+    setup();
+
+    xTaskCreate(&physical_task, "Physical Task", 1500, NULL, 5, NULL);
+
+
+
+    // Send a bluetooth signal 
+
+
 }
